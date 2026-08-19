7655ad64f6e1701acdb9914b1092bcc9c6a2b7d5
blinnikk <ukri.blinnikka@gmail.com>
2026-08-19 19:49:07 +0300

    remove spotify references

--- src/bot/messages.ts
+++ src/bot/messages.ts
@@ -1,6 +1,6 @@
 export const messages = {
     unauthorized: process.env.UNAUTHORIZED_MESSAGE ?? 'Unauthorized',
-    invalidLink: process.env.INVALID_LINK_MESSAGE ?? 'Please send a valid Spotify link.',
+    invalidLink: process.env.INVALID_LINK_MESSAGE ?? 'Please send a valid link.',
     updateSuccess: process.env.UPDATE_SUCCESS_MESSAGE ?? 'Link updated successfully to:',
     updateError: process.env.UPDATE_ERROR_MESSAGE ?? 'Failed to update the link.',
 };
